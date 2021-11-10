96b1b54d4935f57773ce0941b174bc105e329aec
Anders Christiansen Sørby <anders@sorby.xyz>
2021-11-10 13:51:01 +0100

    Add lean and lean-blake3 dependencies

--- flake.nix
+++ flake.nix
@@ -1,25 +1,47 @@
 {
   inputs = {
     utils.url = "github:yatima-inc/nix-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs.url = github:nixos/nixpkgs/nixos-21.05;
+    lean = {
+      url = github:yatima-inc/lean4/acs/add-nix-ability-for-native-libs;
+    };
+    lean-blake3.url = github:yatima-inc/lean-blake3;
+
+    flake-utils = {
+      url = github:numtide/flake-utils;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     { self
     , utils
     , nixpkgs
+    , flake-utils
+    , lean
+    , lean-blake3
     }:
-    utils.inputs.flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (system:
     let
       lib = utils.lib.${system};
       pkgs = nixpkgs.legacyPackages.${system};
+      leanPkgs = lean.packages.${system};
+      Radiya = leanPkgs.buildLeanPackage {
+        src = ./.;
+        name = "Radiya";
+        deps = [ lean-blake3.project.${system} ];
+      };
       inherit (lib) buildRustProject testRustProject rustDefault filterRustProject;
       rust = rustDefault;
       crateName = "radiya";
       root = ./.;
     in
     {
-      packages.${crateName} = buildRustProject { inherit root; };
+      inherit Radiya;
+      packages = {
+        "Radiya" = Radiya.executable;
+        ${crateName} = buildRustProject { inherit root; };
+      };
 
       checks.${crateName} = testRustProject { doCheck = true; inherit root; };
 
